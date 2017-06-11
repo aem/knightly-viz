ae3b1a8bb187b5b9f4f41d02d523b2c0a3a40ea1
aem <amarkon895@gmail.com>
2017-06-11 21:40:59 +0100

    move to google maps api things work now!

--- knightlyViz.js
+++ knightlyViz.js
@@ -3,13 +3,18 @@ let rawData = window.KnightlyData.positions;
 rawData = rawData.filter((el) => new Date(el.created_at).valueOf() > 1484694208617)
 let lat = (rawData.reduce((acc, data) => Math.max(data.lat, acc), -180) + rawData.reduce((acc, data) => Math.min(data.lat, acc), Infinity)) / 2;
 let lng = (rawData.reduce((acc, data) => Math.max(data.lng, acc), -180) + rawData.reduce((acc, data) => Math.min(data.lng, acc), Infinity)) / 2;
-const map = L.map('content').setView([42.3371177,-71.1107741], 14);
-// const map = L.map('content').setView([lng, lat], 14);
-L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoiYWVtIiwiYSI6ImNpeHRodXRkbTAwMWszM251d2w0N2s1dzMifQ.hjAvtPzGfnA-YyPVe37PEQ', {
-		maxZoom: 18,
-		attribution: '',
-		id: 'mapbox.streets'
-}).addTo(map);
-rawData.forEach((obj) => {
-  L.marker(L.latLng(obj.lat, obj.lng)).addTo(map);
+
+const map = new GMaps({
+  div: '#map',
+  lat,
+  lng
 });
+
+rawData.forEach((el) => {
+  map.addMarker({
+    lat: el.lat,
+    lng: el.lng
+  })
+});
+map.fitZoom();
+map.refresh();
