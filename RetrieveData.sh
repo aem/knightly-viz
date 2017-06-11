53e9866097d49e20d59da91ea8317522adc6cf6a
aem <amarkon895@gmail.com>
2017-06-11 21:41:33 +0100

    2 weeks

--- RetrieveData.sh
+++ RetrieveData.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo 'window.KnightlyData = (' > data.js;
-curl "http://knightly.jetru.by/api/public/v1/positions?key=z7JA799689sdh2G7suy8OJJ4ueHsA2o8&from=$(date --date='7 days ago' '+%Y-%m-%d')&to$(date '+%Y-%m-%d')" >> data.js;
+curl "http://knightly.jetru.by/api/public/v1/positions?key=z7JA799689sdh2G7suy8OJJ4ueHsA2o8&from=$(date --date='14 days ago' '+%Y-%m-%d')&to$(date '+%Y-%m-%d')" >> data.js;
 echo ');' >> data.js;
